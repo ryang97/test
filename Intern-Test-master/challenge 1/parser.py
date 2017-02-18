f49d7c8ed7a468c3e950ec08f3b0a509c76edd1d
ryang97 <r52yang@uwaterloo.ca>
2017-02-17 22:14:20 -0500

    table

--- Intern-Test-master/challenge 1/parser.py
+++ Intern-Test-master/challenge 1/parser.py
@@ -12,3 +12,19 @@ for element in tree.iter():
     else:
         print(element.tag, element.text)
 
+
+table = ""
+i = 1
+for element in tree.iter():
+    if element.tag == 'book':
+        table += "</tr>\n<tr>"
+        table += "<td>" + str(i) + "</td>"
+        i = i + 1
+    elif element.tag == 'catalog':
+        i = i
+    elif element.text:
+        table += "<td>" + str(element.text) + "</td>"
+
+table += "</tr>"
+table = table[5:]
+print(table)
